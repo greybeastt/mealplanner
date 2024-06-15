813dcfdcdf2e37ccdce5c1ed8b9998dca332b52f
Ahmed Zein <ahmedzein3652@gmail.com>
2024-06-16 02:12:52 +0300

    ping

--- app.server.js
+++ app.server.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/ping", (req, res) => res.status(200));
 
 app.use("/admin", adminRoute);
 
