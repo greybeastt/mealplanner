c65c2e17e5a8f5170744c8eae7f332fc4db0f30e
Ahmed Zein <ahmedzein3652@gmail.com>
2024-06-16 02:54:49 +0300

    reformat public/auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,42 +1,42 @@
-document
-  .getElementById("login-form")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault(); // Prevent the default form submission
+document.getElementById("login-form").addEventListener("submit", login);
 
-    const form = event.target;
-    const formData = new FormData(form);
+const login = async (event) => {
+  event.preventDefault(); // Prevent the default form submission
 
-    const data = {
-      username: formData.get("username"),
-      password: formData.get("password"),
-    };
-    try {
-      const response = await fetch("/admin/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+  const form = event.target;
+  const formData = new FormData(form);
 
-      if (!response.ok) {
-        const error = await response.json();
-        console.error("Error:", error);
-        // Handle error response
-        alert("Login failed!");
-        return;
-      }
+  const data = {
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+  try {
+    const response = await fetch("/admin/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-      const responseObject = await response.json();
-      if (!responseObject.token) {
-        alert("TOKEN error");
-        return;
-      }
-
-      localStorage.setItem("token", responseObject.token);
-      window.location.replace("/meals.html");
-    } catch (error) {
-      console.error("Network error:", error);
-      alert("Network error occurred.");
+    if (!response.ok) {
+      const error = await response.json();
+      console.error("Error:", error);
+      // Handle error response
+      alert("Login failed!");
+      return;
     }
-  });
+
+    const responseObject = await response.json();
+    if (!responseObject.token) {
+      alert("TOKEN error");
+      return;
+    }
+
+    localStorage.setItem("token", responseObject.token);
+    window.location.replace("/meals.html");
+  } catch (error) {
+    console.error("Network error:", error);
+    alert("Network error occurred.");
+  }
+};
