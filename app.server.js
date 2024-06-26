63468412491e2d3d571246831ba3c692eaf40dca
Ahmed Zein <ahmedzein3652@gmail.com>
2024-06-27 02:28:24 +0300

    small change

--- app.server.js
+++ app.server.js
@@ -9,11 +9,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
+  windowMs: 1 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
 });
 
 // Apply the rate limiting middleware to all requests.
